8c7c11612b23245f51759c37e6c778a99ee48f28
lucasbogadocr7 <lubogado@frba.utn.edu.ar>
2025-08-03 20:38:10 -0300

    renombro carpeta

--- patron_observer/observer/src/main/java/org/example/Administracion.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class Administracion implements Observer {
-    public void actualizar() {
-        System.out.println("Administración notificada: revisar el estado del libro.");
-    }
-}
--- patron_observer/observer/src/main/java/org/example/Compras.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class Compras implements Observer {
-    public void actualizar() {
-        System.out.println("Compras notificada: evaluar reposición del libro.");
-    }
-}
